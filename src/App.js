8d12f92039911a114c76d50be3f090e15b014da5
Benjamin Snellings <development@bensnellings.com>
2019-11-21 22:40:34 -0800

    Add a sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './components/sidebar'
+import praise from './images/PraiseTheSun.jpg'
+import praise2 from './images/praiseit.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <div id="container-wrap">
+            <Sidebar/>
+            <div id="container-main">
+              <header className="App-header">
+                <img src={praise2} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload, PLEASE.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
